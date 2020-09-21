9dbf5b682a61939026b08a574c46b73f10f42d4e
FernandoChecchi <65245824+FernandoChecchi@users.noreply.github.com>
2020-09-21 14:27:53 -0300

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ function App() {
   }
   function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.envAPI_KEY}&units=metric`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.env.API_KEY}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
