3d45a1e4f2657276fa52f2ee06d5546330cc3c21
FernandoChecchi <checchi363@gmail.com>
2020-09-21 13:51:06 -0300

    fisrt commit

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About(){
+    return (
+       <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="h1">About</h1>
+          <span class="lead">Esta pagina fue creada por  </span> 
+          <span class="h4">Fernando</span>
+          <hr class="my-4"></hr>
+          <p class="h6">Utilizando Javascript, Css, Boostrap y React</p>
+        </div>
+       </div>
+    )
+}
